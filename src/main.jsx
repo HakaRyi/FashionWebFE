26adfc86ff1f4aea2b23a2d26e9bfd5ea3031903
hoang2004dev <hoang2004dev@gmail.com>
2026-05-08 02:58:37 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
-// src/main.jsx
-
-import { StrictMode, createElement } from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./app/index";
-
-import "./shared/styles/index.scss";
-import "react-datepicker/dist/react-datepicker.css";
-
-import reportWebVitals from "./reportWebVitals";
-
-createRoot(document.getElementById("root")).render(
-  createElement(
-    StrictMode,
-    null,
-    createElement(App)
-  )
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/index';
+import './shared/styles/index.scss';
+import 'react-datepicker/dist/react-datepicker.css';
+import reportWebVitals from './reportWebVitals';
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        {' '}
+        <App />{' '}
+    </StrictMode>,
 );
-
 reportWebVitals(console.log);
